28718e4c28f4a0390a3fa7e43e9b0c6c7aa5dfde
elena-salam <elena.salamakha.ua@gmail.com>
2021-03-15 22:25:57 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var chars = str.split(''),
+        stack = [],
+        open = ['{', '(', '['],
+        close = ['}', ')', ']'],
+        closeIndex,
+        openIndex;
+
+    // Проходимся по строке, проверяя каждый ее символ (п.4).
+    for (var i = 0, len = chars.length; i < len; i++) {
+       openIndex = open.indexOf(chars[i]);
+       if (openIndex !== -1) {
+           // Нашли открывающую скобку. Помещаем ее в стек (п.2).
+           stack.push(openIndex);
+           continue;
+       }
+
+       closeIndex = close.indexOf(chars[i]);
+       if (closeIndex !== -1) {
+           // Нашли закрывающую скобку. Проверяем ее соответствие открывающей (п.3).
+           openIndex = stack.pop();
+           if (closeIndex !== openIndex) {
+               return false;
+           }
+       }
+    }
+
+    // Проверяем дисбаланс открытых/закрытых скобок (п.5).
+    if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
+
